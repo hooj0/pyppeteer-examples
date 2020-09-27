8c06b3b8ac067c93717ea3a40401f662cb4764be
hoojo <hoojo@qq.com>
2020-09-27 14:22:48 +0800

    :sparkles: :bento: :recycle:  Changed use pyppeteer single task search `douban` book

--- pyppeteer_douban/single_task_search.py
+++ pyppeteer_douban/single_task_search.py
@@ -3,7 +3,7 @@
 # @author: hoojo
 # @email:    hoojo_@126.com
 # @github:   https://github.com/hooj0
-# @create date: 2020-09-22
+# @create date: 2020-09-24
 # @copyright by hoojo @2020
 # @changelog use pyppeteer single task search `douban` book
 
@@ -40,13 +40,29 @@ def screen_size():
     return width, height
 
 
-async def search(text):
+def extract(book_info):
+    nick_name = re.findall(r"副标题:(.*?)\n", book_info)
+    publish_org = re.findall(r"出版社:(.*?)\n", book_info)
+    publish_date = re.findall(r"出版年:(.*?)\n", book_info)
+    price = re.findall(r"定价:(.*?)\n", book_info)
+    isbn = re.findall(r"ISBN:(.*?)\n", book_info)
 
+    return {
+        "nick_name": "".join(nick_name).strip(),
+        "publish_org": "".join(publish_org).strip(),
+        "publish_date": "".join(publish_date).strip(),
+        "price": "".join(price).strip(),
+        "isbn": "".join(isbn).strip()
+    }
+
+
+async def search(text_list):
+
+    # 获取屏幕尺寸
     width, height = screen_size()
-
     # 启动浏览器
-    browser = await launch()
-    # browser = await launch(headless=False, dumpio=True, args=["--disable-infobars", "--start-maximized"])
+    # browser = await launch()
+    browser = await launch(headless=False, dumpio=True, args=["--disable-infobars", "--start-maximized"])
     # 打开一个新页面
     page = await browser.newPage()
     # 设置页面视图大小
@@ -54,74 +70,68 @@ async def search(text):
     # 设置 user-agent 模拟浏览器操作
     await page.setUserAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36")
 
-    # 设置请求URL
-    url = f"https://search.douban.com/book/subject_search?search_text={text}&cat=1001"
-    # 地址栏跳转到当前网址
-    response = await page.goto(url)
-    print("request url: ", response.url)
+    for text in text_list:
+        # 设置请求URL
+        url = f"https://search.douban.com/book/subject_search?search_text={text}&cat=1001"
+        # 地址栏跳转到当前网址
+        response = await page.goto(url)
+        print("request url: ", response.url)
 
-    # 获取网页内容
-    content = await page.content()
-    print("title: %s, content: %s" % (await page.title(), len(content)))
-
-    subject = await page.J("#wrapper div.item-root > a[href^='https://book.douban.com/subject/']")
-    print("subject: ", subject)
-
-    if subject:
-        html = await page.Jeval("#wrapper div.item-root > a[href^='https://book.douban.com/subject/']", "node => node.outerHTML")
-        print("subject html: ", html)
-        print("\n\n")
-
-        # 等待新页面加载完成
-        await asyncio.gather(
-            page.waitForNavigation(),
-            subject.click(),
-        )
-
-        title = await page.title()
+        # 获取网页内容
         content = await page.content()
-        print("title: %s, content: %s" % (title, len(content)))
+        print("search: %s, content: %s" % (await page.title(), len(content)))
 
-        name = await page.Jeval("div#wrapper h1", "node => node.innerText")
-        author = await page.Jeval("div#wrapper div#info a[href^='/author/']", "node => node.innerText")
-        nick_name = await page.Jeval("div#wrapper div#info", "node => node.textContent")
-        # nick_name = nick_name.replace(" ", "").replace("\n", "")
+        subject = await page.J("#wrapper div.item-root > a[href^='https://book.douban.com/subject/']")
+        if subject:
+            # html = await page.Jeval("#wrapper div.item-root > a[href^='https://book.douban.com/subject/']", "node => node.outerHTML")
+            # print("subject html: ", html)
 
-        p = re.findall(r'副标题:(.*?)原作名', nick_name, re.M|re.S)
-        if p:
-            print(p[0].strip())
+            # 等待新页面加载完成
+            await asyncio.gather(
+                page.waitForNavigation(),
+                subject.click(),
+            )
 
-        m = re.search(r'副标题:(.*?)原作名', nick_name, re.M|re.S)
-        if m:
-            print(m)
-            print(m.groups())
-            print(m.span())
-            print(m.group(0).strip())
-            print(m.group(1).strip())
+            title = await page.title()
+            content = await page.content()
+            print("title: %s, content: %s" % (title, len(content)))
 
-            nick_name = m.group(1).strip()
+            interest_sectl = await page.J("div#interest_sectl")
+            if not interest_sectl:
+                print("非图书，无法提取到相关记录：%s\n" % text)
+                continue
 
-        score = await page.Jeval("div#interest_sectl strong.rating_num", "node => node.innerText")
-        star = await page.Jeval("div#interest_sectl div.rating_right div.ll", "node => node.className.replace('ll bigstar', '')")
-        num = await page.Jeval("div#interest_sectl div.rating_right a.rating_people", "node => node.innerText")
+            name = await page.Jeval("div#wrapper h1", "node => node.innerText")
+            author = await page.Jeval("div#wrapper div#info a[href]", "node => node.innerText")
 
-        tags = await page.Jeval("div#wrapper div#db-tags-section div.indent", "node => node.innerText")
+            score = await page.Jeval("div#interest_sectl strong.rating_num", "node => node.innerText")
+            star = await page.Jeval("div#interest_sectl div.rating_right div.ll", "node => node.className.replace('ll bigstar', '')")
+            num = await page.Jeval("div#interest_sectl div.rating_right a.rating_people", "node => node.innerText")
 
-        book = {
-            "name": name, "author": author, "nick_name": nick_name,
-            "score": score, "star": star, "num": num,
-            "tags": tags.replace(" ", "").replace("\xa0", ", ")
-        }
+            tags = await page.Jeval("div#wrapper div#db-tags-section div.indent", "node => node.innerText")
 
-        print("book info: ", book)
-    else:
-        print("没有搜索到相关记录：", text)
+            book_info_text = await page.Jeval("div#wrapper div#info", "node => node.textContent")
+            book_info = extract(book_info_text)
+            print("book info: ", book_info)
 
-    # await asyncio.sleep(100)
+            book = {
+                "name": name, "author": author,
+                "score": score, "star": star, "num": num,
+                "tags": tags.replace(" ", "").replace("\xa0", ", ")
+            }
+
+            print("book: ", book)
+        else:
+            print("没有搜索到相关记录：", text)
+
+        print("\n")
+        await asyncio.sleep(2)
+
+    await asyncio.sleep(100)
     # 关闭浏览器
     await browser.close()
 
+
 # 利用异步方式执行函数
-text = "afafdafd"
-text = "传"
-asyncio.get_event_loop().run_until_complete(search(text))
+text_list = ["传", "afafdafd", "人民", "小狗钱钱"]
+asyncio.get_event_loop().run_until_complete(search(text_list))
